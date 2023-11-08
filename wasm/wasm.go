61d93201a6eab360904f56bd09821219bbad9a61
Arif Dogan <arifcodes34@gmail.com>
2023-11-08 17:52:27 +0100

    wasm go commented out

--- wasm/wasm.go
+++ wasm/wasm.go
@@ -9,10 +9,12 @@ import (
 	"nhooyr.io/websocket"
 )
 
+// Conn wraps a WebSocket connection.
 type Conn struct {
 	wsConn *websocket.Conn
 }
 
+// NewConn establishes a new WebSocket connection to a specified URL.
 func NewConn() *Conn {
 	c, _, err := websocket.Dial(context.Background(), "ws://localhost:8080/ws", nil)
 	if err != nil {
@@ -23,23 +25,30 @@ func NewConn() *Conn {
 		wsConn: c,
 	}
 }
+
 func main() {
+	// Channel to keep the main function running until it's closed.
 	c := make(chan struct{}, 0)
 
 	println("WASM Go Initialized")
-	// register functions
+	// Establish a new WebSocket connection.
 	conn := NewConn()
 
+	// Register the logMessage function in the global JavaScript context.
 	js.Global().Set("logMessage", logMessageFunc(conn))
 
+	// Start reading messages in a new goroutine.
 	go conn.readMessage()
+
+	// Wait indefinitely.
 	<-c
 }
 
+// logMessageFunc returns a js.Func that sends a "HELLO" message over WebSocket when invoked.
 func logMessageFunc(conn *Conn) js.Func {
 	return js.FuncOf(func(this js.Value, args []js.Value) interface{} {
 		println("Button Clicked!")
-		// Use the Conn instance that was passed in when creating this function
+		// Send a message through the WebSocket connection.
 		err := conn.wsConn.Write(context.Background(), websocket.MessageText, []byte("HELLO"))
 		if err != nil {
 			log.Println("Error writing to WebSocket:", err)
@@ -48,28 +57,37 @@ func logMessageFunc(conn *Conn) js.Func {
 	})
 }
 
+// readMessage handles incoming WebSocket messages and updates the DOM accordingly.
 func (c *Conn) readMessage() {
 	defer func() {
+		// Close the WebSocket connection when the function returns.
 		c.wsConn.Close(websocket.StatusGoingAway, "BYE")
 	}()
 
 	for {
+		// Read a message from the WebSocket connection.
 		messageType, payload, err := c.wsConn.Read(context.Background())
 
 		if err != nil {
+			// Log and panic if there is an error reading the message.
 			log.Panicf(err.Error())
 		}
 
+		// Update the DOM with the received message.
 		updateDOMContent(string(payload))
 
+		// Log the message type and payload for debugging.
 		log.Println("MessageType: ", messageType)
 		log.Println("Payload: ", string(payload))
 	}
-
 }
 
+// updateDOMContent updates the text content of the DOM element with the given text.
 func updateDOMContent(text string) {
+	// Get the document object from the global JavaScript context.
 	document := js.Global().Get("document")
+	// Get the DOM element by its ID.
 	element := document.Call("getElementById", "count")
+	// Set the innerText of the element to the provided text.
 	element.Set("innerText", text)
 }
